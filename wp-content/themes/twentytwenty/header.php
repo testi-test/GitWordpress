f8f6e26229b36ed0a27ff0caf4488b0c90389f66
testi-test <67255070+testi-test@users.noreply.github.com>
2020-06-23 14:16:27 +0530

    Update header.php

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -63,7 +63,7 @@
 							twentytwenty_site_logo();
 
 							// Site description.
-							twentytwenty_site_description();
+							//twentytwenty_site_description();
 						?>
 
 					</div><!-- .header-titles -->
